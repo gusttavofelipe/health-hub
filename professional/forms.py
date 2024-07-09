29978fb869cb368648124468cefd7dfa3fb3a8a5
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2024-07-09 11:22:32 -0300

    feat: add professional register form

--- professional/forms.py
+++ professional/forms.py
@@ -2,7 +2,19 @@ from django import forms
 from professional.models import Professional
 
 
-class PersonalHealthRecordForm(forms.ModelForm):
+class ProfessionalForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    confirm_password = forms.CharField(
+        label="Confirm password", widget=forms.PasswordInput
+    )
+
     class Meta:
         model = Professional
-        fields = ["full_name", "phone_number", "email", "birth_date"]
+        fields = [
+            "full_name",
+            "phone_number",
+            "email",
+            "birth_date",
+            "password",
+            "confirm_password",
+        ]
